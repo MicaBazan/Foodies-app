9ce93374734b60a54df0144bdcf558b069ce4e80
MicaBazan <micabazan5@gmail.com>
2025-04-08 13:44:24 -0300

    addes logo and icons

--- adminpanel/src/assets/assets.js
+++ adminpanel/src/assets/assets.js
@@ -2,8 +2,8 @@ import logo from './logo.jpg'
 import parcel from './parcel.avif'
 import upload from './upload.png'
 
-export const assets = [
+export const assets = {
     logo,
     parcel,
     upload
-];
+};
