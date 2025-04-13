589bcaf2c24e5785f50293c71085b86b0ec99efb
MicaBazan <micabazan5@gmail.com>
2025-04-13 12:52:50 -0300

    categories

--- foodies/src/components/ExploreMenu/ExploreMenu.jsx
+++ foodies/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,10 +6,22 @@ const ExploreMenu = () => {
     <div className="explore-menu position-relative">
         <h1 className="d-flex aling-items-center justify-content-between">
             Explore Our Menu
+            <div className="d-flex">
+                <i className='bi bi-arrow-left-circle scroll-icon'></i>
+                <i className='bi bi-arrow-right-circle scroll-icon'></i>
+            </div>
         </h1>
-        <div className="d-flex">
-            <i className='bi bi-arrow-left-circle scroll-icon'></i>
-            <i className='bi bi-arrow-right-circle scroll-icon'></i>
+        <p>Explore curated list of dishes from top categories</p>
+        <div className="d-flex justify-content-between gap-4 overflow-auto explore-menu-list">
+          {
+            categories.map((item, index) => {
+              return (
+                <div className="text-center explore-menu-list-item">
+                  
+                </div>
+              )
+            })
+          }
         </div>
     </div>
   )
